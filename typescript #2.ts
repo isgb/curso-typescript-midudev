d18114d148a35ff629805a69de2cff484ed7eefb
oscar <oscar.vasquez@zentro.com.mx>
2024-06-07 16:20:03 -0600

    debug

--- typescript #2.ts
+++ typescript #2.ts
@@ -8,21 +8,31 @@
 //     FORBIDDEN
 // }
 
-const enum ERROR_TYPES {
-    NOT_FOUND = 'notFound',
-    UNAUTHORIZED = 'unauthorized',
-    FORBIDDEN = 'forbidden',
-}
+// const enum ERROR_TYPES {
+//     NOT_FOUND = 'notFound',
+//     UNAUTHORIZED = 'unauthorized',
+//     FORBIDDEN = 'forbidden',
+// }
 
-function mostrarMensaje(tipoDeError){
-    if(tipoDeError === ERROR_TYPES.NOT_FOUND){
-        console.log("no se encuentra el recurso");
+// function mostrarMensaje(tipoDeError){
+//     if(tipoDeError === ERROR_TYPES.NOT_FOUND){
+//         console.log("no se encuentra el recurso");
         
-    } else if(tipoDeError === ERROR_TYPES.UNAUTHORIZED){
-        console.log("no tienes permisos para acceder");
+//     } else if(tipoDeError === ERROR_TYPES.UNAUTHORIZED){
+//         console.log("no tienes permisos para acceder");
         
-    } else if(tipoDeError === ERROR_TYPES.FORBIDDEN){
-        console.log("no tienes permisos para acceder");
+//     } else if(tipoDeError === ERROR_TYPES.FORBIDDEN){
+//         console.log("no tienes permisos para acceder");
         
-    }
-}
+//     }
+// }
+
+/**************************************** */
+// Aserciones de tipos
+// const canvas = document.getElementById('span')
+
+// // es inferencia->Typescript se da cuenta que dentro del if
+// // ya solo el canvas va a poder ser un HTMLCanvasElement
+// if(canvas instanceof HTMLCanvasElement){
+//     const ctx = canvas.getContext('2d');
+// }
